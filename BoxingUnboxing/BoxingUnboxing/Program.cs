9404e00d73c412687ec9966e15890093704fd86b
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-19 07:12:44 +0530

    Add: BoxingUnboxing

---
+++ BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 23;
+
+            //! boxing (Convert Value Type to refrence type)
+            object obj = num;
+
+            //! unboxing (Convert refrence Type to Value type
+            int i = (int)obj;
+
+            //! Display result
+            Console.WriteLine("Value of ob object is : " + obj);
+            Console.WriteLine("Value of i is : " + i);
+        }
+    }
+    }
+}
