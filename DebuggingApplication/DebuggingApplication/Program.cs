10cff2f9041de8d2f3bbefaeb51d10a54afc4870
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-20 07:45:57 +0530

    Add: DebuggingApplication

---
+++ DebuggingApplication/DebuggingApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DebuggingApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? userResponse;
+            userResponse = Console.ReadLine();
+            if (Convert.ToBoolean(int.Parse(userResponse)))
+            {
+                Console.WriteLine(userResponse);
+            }
+        }
+    }
+}
