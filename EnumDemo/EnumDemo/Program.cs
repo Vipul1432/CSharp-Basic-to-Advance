b2d839cc1974ff8d469ac8271825171829562e3f
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-20 07:50:10 +0530

    Add: EnumDemo

---
+++ EnumDemo/EnumDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EnumDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WeekDays.Monday);
+            Console.WriteLine((int)WeekDays.Sunday);
+            Console.WriteLine((int)WeekDays.Tuesday);
+            Console.WriteLine((int)WeekDays.Wednesday);
+            Console.WriteLine(WeekDays.Thursday);
+            Console.WriteLine((int)WeekDays.Friday);
+            Console.WriteLine((int)WeekDays.Saturday);
+
+            Console.ReadKey();
+        }
+    }
+
+    public enum WeekDays
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
