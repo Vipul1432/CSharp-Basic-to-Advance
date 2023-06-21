48098fe21c4d8d2ebe5e2d9e13dd3d7fb18bf4f3
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-21 08:01:51 +0530

    Add: ExceptionDemo

---
+++ ExceptionDemo/ExceptionDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ExceptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1, Number2, Result;
+            try
+            {
+                Console.WriteLine("Enter First Number:");
+                Number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number:");
+                Number2 = int.Parse(Console.ReadLine());
+                Result = Number1 / Number2;
+                Console.WriteLine($"Result = {Result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Source: {ex.Source}");
+                Console.WriteLine($"HelpLink: {ex.HelpLink}");
+                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+            }
+        }
+    }
+}
