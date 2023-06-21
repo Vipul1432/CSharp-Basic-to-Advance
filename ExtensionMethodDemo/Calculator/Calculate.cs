3a05c1890a1a53f3c50deb31aa4340a1d1a4fec8
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-21 08:02:25 +0530

    Add: ExtensionMethodDemo

---
+++ ExtensionMethodDemo/Calculator/Calculate.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator
+{
+    public class Calculate
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine($"The sum is {a + b}");
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine($"The Difference is {a - b}");
+        }
+    }
+}
---
+++ ExtensionMethodDemo/ExtensionMethodDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Calculator;
+
+namespace ExtensionMethodDemo
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = "This is a string";
+            Console.WriteLine(str.WordCount());
+        }
+
+        //! Extension Method of Calculator Class Libary
+       public static int WordCount(this string str)
+        {
+            return str.Split(" ").Length;
+
+        }
+    }
+}
