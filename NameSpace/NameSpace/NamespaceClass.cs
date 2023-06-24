597789f9482b652c8e939d8b6cca8dc0aad83760
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-24 10:33:46 +0530

    Add: NameSpace

---
+++ NameSpace/NameSpace/NamespaceClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NameSpace
+{
+    internal class NamespaceClass
+    {
+        public void NamespaceMethod() {
+            Console.WriteLine("This is Name space class Method");
+        }
+    }
+}
---
+++ NameSpace/NameSpace/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using First;
+using NameSpace;
+namespace First
+{
+    public class Hello
+    {
+        public void SayHello() { Console.WriteLine("Hello First Namespace"); }
+    }
+}
+namespace Second
+{
+    public static class Hello
+    {
+        public static void SayHello() { Console.WriteLine("Hello Second Namespace"); }
+    }
+}
+namespace TestNameSpace
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Create instance and access it
+            Hello h1 = new();
+            NamespaceClass nc = new();
+
+            // Calling method using object instance
+            h1.SayHello();
+            nc.NamespaceMethod();
+
+            // Accessing static method in static class using class name
+            Second.Hello.SayHello();
+        }
+    }
+}
