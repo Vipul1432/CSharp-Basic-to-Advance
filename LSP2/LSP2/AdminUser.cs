9caafab6856e8b1fb477ec9246d9d55adb28d8fb
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-24 10:31:48 +0530

    Add: LSP2

---
+++ LSP2/LSP2/AdminUser.cs
@@ -0,0 +1,41 @@
+﻿namespace LSP2
+{
+    internal class AdminUser
+    {
+        public virtual void Write()
+        {
+            Console.WriteLine("Admin user Write");
+        }
+        public virtual void Read()
+        {
+            Console.WriteLine("Admin User Read");
+        }
+    }
+    internal class NormalUser
+    {
+        public virtual void Read()
+        {
+            Console.WriteLine("Normal User Read");
+        }
+    }
+
+    internal class AdminUserAccessData : AdminUser
+    {
+        public override void Read()
+        {
+            Console.WriteLine("Normal User Access data");
+        }
+        public override void Write()
+        {
+            Console.WriteLine("Normal User Access data");
+        }
+    }
+    internal class NormalUserAccessData : NormalUser
+    {
+        public override void Read()
+        {
+            Console.WriteLine("Normal User Access data");
+        }
+    }
+}
+
---
+++ LSP2/LSP2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LSP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NormalUser nu = new NoramalUserDataAccess();
+            nu.Read();
+
+        }
+    }
+}
