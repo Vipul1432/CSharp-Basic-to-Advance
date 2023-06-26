02770cc8a4c8dd7038107d044047507f30398086
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:16:32 +0530

    Add: ClassTest

---
+++ ClassTest/ClassTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ClassTutorial
+{
+    public class ClassTest
+    {
+        private int id;//data member (also instance variable)    
+        string? name;//data member(also instance variable)    
+
+        public static void Main(string[] args)
+        {
+            ClassTest obj = new();
+            obj.id = 101;
+            obj.name = "Vipul Kumar"; 
+            
+            Console.WriteLine(obj.id);
+            Console.WriteLine(obj.name);
+
+        }
+    }
+}
