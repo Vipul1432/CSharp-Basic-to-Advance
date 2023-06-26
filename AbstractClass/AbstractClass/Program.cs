88061a4ff8bcc6c34303046bcb30252e09e7826f
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:08:09 +0530

    Add AbstractClass

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbsChild absChild = new();
+            absChild.Add(10, 5);
+            absChild.Sub(10, 5);
+            absChild.Mul(10, 5);
+            absChild.Div(10, 2);
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class AbsParent
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine($"Addition of {x} and {y} is : {x + y}");
+        }
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine($"Subtraction of {x} and {y} is : {x - y}");
+        }
+        public abstract void Mul(int x, int y);
+        public abstract void Div(int x, int y);
+    }
+
+    public class AbsChild : AbsParent
+    {
+        public override void Mul(int x, int y)
+        {
+            Console.WriteLine($"Multiplication of {x} and {y} is : {x * y}");
+        }
+        public override void Div(int x, int y)
+        {
+            Console.WriteLine($"Division of {x} and {y} is : {x / y}");
+        }
+    }
+}
