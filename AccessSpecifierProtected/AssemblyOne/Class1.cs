cb1d203ebeeb97874befa1afbeb6a15072cb7f72
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:11:03 +0530

    Add: AccessSpecifierProtected

---
+++ AccessSpecifierProtected/AssemblyOne/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace AssemblyOne
+{
+    public class AssemblyOneClass1
+    {
+        protected int Id;
+        public void Display1()
+        {
+            //protected Members Accessible with the Containing Type 
+            //Where they are created
+            Console.WriteLine(Id);
+        }
+    }
+    public class AssemblyOneClass2 : AssemblyOneClass1
+    {
+        public void Display2()
+        {
+            //We Can access protected Member from Derived Classes
+            //Within the Same Assembly
+            Console.WriteLine(Id); //No-Compile Time Error
+        }
+    }
+
+    public class AssemblyOneClass3
+    {
+        public void Dispplay3()
+        {
+            //We Cannot access protected Member from Non-Derived Classes
+            //Within the Same Assembly
+            AssemblyOneClass1 obj = new AssemblyOneClass1();
+            Console.WriteLine(obj.Id); //Compile Time Error
+        }
+    }
+}
