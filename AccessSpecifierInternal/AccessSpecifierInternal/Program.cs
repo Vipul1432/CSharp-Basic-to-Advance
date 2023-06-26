d7268e40adc6d258418377e6fff8811a5efb34f8
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:10:08 +0530

    Add: AccessSpecifierInternal

---
+++ AccessSpecifierInternal/AccessSpecifierInternal/Program.cs
@@ -0,0 +1,32 @@
+﻿using AssemblyOne;
+using System;
+namespace AccessSpecifierDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class AnotherAssemblyClass1 : AssemblyOneClass1
+    {
+        public void Display4()
+        {
+            //We cannot access the Internal Member from Derived Classes
+            //from Other Assemblies
+            Console.WriteLine(Id); //Compile Time Error
+        }
+    }
+
+    public class AnotherAssemblyClass2
+    {
+        public void Dispplay3()
+        {
+            //We cannot access internal Member from Non-Derived Classes
+            //from Other Assemblies
+            AssemblyOneClass1 obj = new AssemblyOneClass1();
+            Console.WriteLine(obj.Id); //Compile Time Error
+        }
+    }
+}
---
+++ AccessSpecifierInternal/AssemblyOne/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace AssemblyOne
+{
+    public class AssemblyOneClass1
+    {
+        internal int Id;
+        public void Display1()
+        {
+            //internal Members Accessible with the Containing Type 
+            //Where they are created
+            Console.WriteLine(Id);
+        }
+    }
+    public class AssemblyOneClass2 : AssemblyOneClass1
+    {
+        public void Display2()
+        {
+            //We can access internal Members from Derived Classes
+            //Within the Same Assembly
+            Console.WriteLine(Id); //No-Compile Time Error
+        }
+    }
+
+    public class AssemblyOneClass3
+    {
+        public void Dispplay3()
+        {
+            //We can access internal Members from Non-Derived Classes
+            //Within the Same Assembly
+            AssemblyOneClass1 obj = new AssemblyOneClass1();
+            Console.WriteLine(obj.Id); //No-Compile Time Error
+        }
+    }
+}
