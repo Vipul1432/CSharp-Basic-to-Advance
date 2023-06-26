8e10e90959bc3aac7c1cf82dcd4b5f024c8d169a
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:16:01 +0530

    Add: Class

---
+++ Class/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Student
+{
+    int id; //data member (also instance variable)
+    string? name; //data member(also instance variable)
+
+    public static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.id = 153;
+        s1.name = "Vipul Kumar";
+        Console.WriteLine(s1.id);
+        Console.WriteLine(s1.name);
+    }
+}
