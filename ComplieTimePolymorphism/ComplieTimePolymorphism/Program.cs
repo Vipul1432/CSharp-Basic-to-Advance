0f38b00dfd30df193c99f383d38919a3adb733e5
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:17:20 +0530

    Add: ComplieTimePolymorphism

---
+++ ComplieTimePolymorphism/ComplieTimePolymorphism/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ComplieTimePolymorphism
+{
+    class Program
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        // Method Overloading
+        public void Add(float x, float y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void Add(string s1, string s2)
+        {
+            Console.WriteLine(s1 + " " + s2);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Add(10, 20);
+            obj.Add(10.5f, 20.5f);
+            obj.Add("Vipul", "Kumar");
+            Console.ReadKey();
+        }
+    }
+}
