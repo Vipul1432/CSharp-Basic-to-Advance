7a4ca97c176068f1d78f3c45c93e6a017d1979ef
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:12:16 +0530

    Add: AccessSpecifierProtectedInternal

---
+++ AccessSpecifierProtectedInternal/AccessSpecifierProtectedInternal/Program.cs
@@ -0,0 +1,32 @@
+﻿using AssemblyOne;
+using System;
+namespace AccessSpecifierProtectedInternal
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class AnotherAssemblyClass1 : AssemblyOneClass1
+    {
+        public void Display4()
+        {
+            //We can access the protected internal Members from Derived Classes
+            //from Other Assemblies
+            Console.WriteLine(Id); //No-Compile Time Error
+        }
+    }
+
+    public class AnotherAssemblyClass2
+    {
+        public void Dispplay3()
+        {
+            //We cannot access protected internal Members from Non-Derived Classes
+            //from Other Assemblies
+            AssemblyOneClass1 obj = new AssemblyOneClass1();
+            Console.WriteLine(obj.Id); //Compile Time Error
+        }
+    }
+}
---
+++ AccessSpecifierProtectedInternal/SsemblyOne/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace AssemblyOne
+{
+    public class AssemblyOneClass1
+    {
+        protected internal int Id;
+        public void Display1()
+        {
+            //protected internal Members Accessible with the Containing Type 
+            //Where they are created
+            Console.WriteLine(Id);
+        }
+    }
+    public class AssemblyOneClass2 : AssemblyOneClass1
+    {
+        public void Display2()
+        {
+            //We can access protected internal Member from Derived Classes
+            //Within the Same Assembly
+            Console.WriteLine(Id); //No-Compile Time Error
+        }
+    }
+
+    public class AssemblyOneClass3
+    {
+        public void Dispplay3()
+        {
+            //We can access protected internal Member from Non-Derived Classes
+            //Within the Same Assembly
+            AssemblyOneClass1 obj = new AssemblyOneClass1();
+            Console.WriteLine(obj.Id); //No-Compile Time Error
+        }
+    }
+}
