08a1046ab5600c25c3ba02cf8afa6be021c86033
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:09:12 +0530

    Add: Abstracton

---
+++ Abstracton/Abstracton/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+namespace AbstractionUsingInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Transaction doing SBI Bank");
+            IBank sbi = BankFactory.GetBankObject("SBI");
+            sbi.ValidateCard();
+            sbi.WithdrawMoney();
+            sbi.CheckBalanace();
+            sbi.BankTransfer();
+            sbi.MiniStatement();
+
+            Console.WriteLine("\nTransaction doing AXIX Bank");
+            IBank AXIX = BankFactory.GetBankObject("AXIX");
+            AXIX.ValidateCard();
+            AXIX.WithdrawMoney();
+            AXIX.CheckBalanace();
+            AXIX.BankTransfer();
+            AXIX.MiniStatement();
+
+            Console.Read();
+        }
+    }
+
+    public interface IBank
+    {
+        void ValidateCard();
+        void WithdrawMoney();
+        void CheckBalanace();
+        void BankTransfer();
+        void MiniStatement();
+    }
+
+    public class BankFactory
+    {
+        public static IBank GetBankObject(string bankType)
+        {
+            IBank BankObject = null;
+            if (bankType == "SBI")
+            {
+                BankObject = new SBI();
+            }
+            else if (bankType == "AXIX")
+            {
+                BankObject = new AXIX();
+            }
+            return BankObject;
+        }
+    }
+
+    public class SBI : IBank
+    {
+        public void BankTransfer()
+        {
+            Console.WriteLine("SBI Bank Bank Transfer");
+        }
+
+        public void CheckBalanace()
+        {
+            Console.WriteLine("SBI Bank Check Balanace");
+        }
+
+        public void MiniStatement()
+        {
+            Console.WriteLine("SBI Bank Mini Statement");
+        }
+
+        public void ValidateCard()
+        {
+            Console.WriteLine("SBI Bank Validate Card");
+        }
+
+        public void WithdrawMoney()
+        {
+            Console.WriteLine("SBI Bank Withdraw Money");
+        }
+    }
+
+    public class AXIX : IBank
+    {
+        public void BankTransfer()
+        {
+            Console.WriteLine("AXIX Bank Bank Transfer");
+        }
+
+        public void CheckBalanace()
+        {
+            Console.WriteLine("AXIX Bank Check Balanace");
+        }
+
+        public void MiniStatement()
+        {
+            Console.WriteLine("AXIX Bank Mini Statement");
+        }
+
+        public void ValidateCard()
+        {
+            Console.WriteLine("AXIX Bank Validate Card");
+        }
+
+        public void WithdrawMoney()
+        {
+            Console.WriteLine("AXIX Bank Withdraw Money");
+        }
+    }
+}
