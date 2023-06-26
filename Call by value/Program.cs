49dfd1ecfe27b2d37c073f7c62e4fe34a7d1030e
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:15:32 +0530

    Add: Call by value

---
+++ Call by value/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MyApplication
+{
+    class Program
+    {
+        public static void squareFun(int a)
+        {
+            int result = a * a;
+            System.Console.WriteLine("Square of {0} is " + result, a);
+        }
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter value of a :  ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            squareFun(a);
+        }
+    }
+}
