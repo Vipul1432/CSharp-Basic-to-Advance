5bfc862eebb16282036fed369e1945cf12cc64b4
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-26 08:15:06 +0530

    Add: Call by Reference

---
+++ Call by Reference/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            System.Console.Write("Enter a number : ");
+            int num = Convert.ToInt32(System.Console.ReadLine());
+            int result = 1;
+            Factorial(ref num, ref result);
+            System.Console.WriteLine("Factorial of {0} is {1}", num, result);
+        }
+
+        public static void Factorial(ref int num, ref int result)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+        }
+    }
+}
