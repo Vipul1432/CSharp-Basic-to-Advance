96993a78bf1a721fc50f1d8cad880f54b0956cd3
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-27 07:49:23 +0530

    Add: ConcreteClass

---
+++ ConcreteClass/ConcreteClass/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConcreteClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new();
+            human.Talk();
+            human.Move();
+            Dog dog = new();
+            dog.Talk();
+            dog.Move();
+        }
+    }
+
+    public abstract class Animal
+    {
+        public abstract void Talk();
+        public abstract void Move();
+    }
+
+    // Concrete Class 
+    public class Human : Animal
+    {
+        public override void Talk()
+        {
+            Console.WriteLine("This blog post is awesome! :)");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("I'm walking on 2 feet.");
+        }
+    }
+
+    // Concrete Class 
+    public class Dog : Animal
+    {
+        public override void Talk()
+        {
+            Console.WriteLine("Woof! WOOF!");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("I'm walking on 4 feet.");
+        }
+
+    }
+}
