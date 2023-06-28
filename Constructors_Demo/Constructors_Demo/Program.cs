9bc9e9bb1cd8bdeac0e486293873741a038e564c
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-28 07:42:02 +0530

    Add: Constructors_Demo

---
+++ Constructors_Demo/Constructors_Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Constructors_Demo
+{
+    class Customer
+    {
+        // Member Variables
+        public string Name;
+
+        //Constructor for initializing fields
+        public Customer(string fname, string lname)
+        {
+            Name = fname + " " + lname;
+        }
+        //method for displaying customer records
+        public void AppendData()
+        {
+            Console.WriteLine(Name);
+        }
+        //Entry point
+        static void Main(string[] args)
+        {
+            // object instantiation
+            Customer obj = new Customer("Vipul", "Kumar");
+
+            //Method calling
+            obj.AppendData();
+        }
+    }
+}
