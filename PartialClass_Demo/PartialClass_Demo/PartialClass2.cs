a26b76f567e8ba740577196c2d29310767ef6346
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-28 07:57:01 +0530

    Add: PartialClass_Demo

---
+++ PartialClass_Demo/PartialClass_Demo/PartialClass2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartialClass_Demo
+{
+    public partial class PartialClass
+    {
+        public void FullName()
+        {
+            Console.WriteLine("Full Name:" + FirstName + " " + LastName);
+        }
+    }
+}
