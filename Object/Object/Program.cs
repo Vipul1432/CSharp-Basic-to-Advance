da566140e50094554b1168aed29f5c53811c9a18
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-28 07:54:01 +0530

    Add: Object

---
+++ Object/Object/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Object
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + firstName +" " +lastName);
+            Console.WriteLine("I am "+ age + "years old");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Person();
+            obj.firstName = "Vipul";
+            obj.lastName = "Kumar";
+            obj.age = 21;
+            obj.Introduce();
+        }
+    }
+}
