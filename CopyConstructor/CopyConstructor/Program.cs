1c74e18a11a972042bd0746024554bac16a584fb
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-28 07:43:13 +0530

    Add: CopyConstructor

---
+++ CopyConstructor/CopyConstructor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace CopyConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CopyConstructor obj1 = new CopyConstructor(10);
+            obj1.Display();
+            CopyConstructor obj2 = new CopyConstructor(obj1);
+            obj2.Display();
+            Console.ReadKey();
+        }
+    }
+
+    public class CopyConstructor
+    {
+        int x;
+
+        //Parameterized Constructor
+        public CopyConstructor(int i)
+        {
+            x = i;
+        }
+
+        //Copy Constructor
+        public CopyConstructor(CopyConstructor obj)
+        {
+            x = obj.x;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Value of X = {x}");
+        }
+    }
+}
