698f3f9ccc981b516dd4225b6e530e745ee3c33e
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-28 07:47:53 +0530

    Add: InheritanceDemo

---
+++ InheritanceDemo/InheritanceDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace InheritanceDemo
+{
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("Class A Constructor is Called");
+        }
+        public void Method1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+    class B : A
+    {
+        B()
+        {
+            Console.WriteLine("Class B Constructor is Called");
+        }
+        public void Method3()
+        {
+            Console.WriteLine("Method 3");
+        }
+        static void Main()
+        {
+            B obj = new B();
+            obj.Method1();
+            obj.Method2();
+            obj.Method3();
+            Console.ReadKey();
+        }
+    }
+}
