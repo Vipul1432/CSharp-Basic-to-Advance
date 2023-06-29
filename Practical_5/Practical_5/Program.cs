14c71b415763beeb723b4c1699e3a497b842a843
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-29 07:31:22 +0530

    Add: Practical_5

---
+++ Practical_5/Practical_5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace Practical_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int YourChoice;
+            Student student = new Student();
+            do
+            {
+                Console.Write("Enter Student Name : ");
+                student.Name = Console.ReadLine();
+            } while ((!Regex.IsMatch(student.Name, @"^[\p{L} \.\-]+$")));
+            for(int i = 0; i <5; i++)
+            {
+                do
+                {
+                    Console.Write($"Enter {i+1} Subject Marks : ");
+                } while(!(decimal.TryParse(Console.ReadLine(), out student.Marks[i]) && student.Marks[i] <=100 && student.Marks[i] >= 0));
+            }
+            Console.WriteLine("Select 1,2,3,4");
+            Console.WriteLine("1 - Aggregate");
+            Console.WriteLine("2 - MinMark");
+            Console.WriteLine("3 - MaximumMark");
+            Console.WriteLine("4 - Grade");
+            do
+            {
+                Console.Write($"Enter Your Choice : ");
+            } while (!(int.TryParse(Console.ReadLine(), out YourChoice) && YourChoice <= 4 && YourChoice >= 0));
+         
+            if(YourChoice == (int)Options.Aggregate)
+            {
+                student.CalculateAverageMarks();
+                Console.WriteLine($"{student.Name} : {Student.AverageMarks}");
+            }
+            if (YourChoice == (int)Options.MinMark)
+            {
+                Console.WriteLine(student.Marks.Min());
+            }
+            if (YourChoice == (int)Options.MinMark)
+            {
+                Console.WriteLine(student.Marks.Max());
+            }
+            if (YourChoice == (int)Options.Grade)
+            {
+                //Console.WriteLine(student.Marks.Max());
+            }
+        }
+    }
+}
---
+++ Practical_5/Practical_5/Student.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Practical_5
+{
+    internal class Student
+    {
+        public string Name;
+        public decimal[] Marks = new decimal[5];
+        public static decimal AverageMarks;
+        public decimal CalculateAverageMarks()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                AverageMarks += Marks[i];
+            }
+            return AverageMarks;
+        }
+        public string CalculateGrade(decimal marks) =>
+            marks switch
+            {
+                > 90 => "A",
+                > 80 => "B",
+                > 70 => "C",
+                < 70 => "D",
+                _ => "Invalid Data",
+            };
+    }
+    enum Options
+    {
+        Aggregate = 1,
+        MinMark = 2,
+        MaximumMark = 3,
+        Grade = 4
+    }
+}
