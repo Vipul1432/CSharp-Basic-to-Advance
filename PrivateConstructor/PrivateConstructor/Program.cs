182866638e6e974be5ff942a419ee2b00f87a48f
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-29 07:32:20 +0530

    Add: PrivateConstructor

---
+++ PrivateConstructor/PrivateConstructor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PrivateConstructor
+{
+    class Program
+    {
+        private Program()
+        {
+            Console.WriteLine("This is private constructor");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("Main method");
+            Console.ReadKey();
+        }
+    }
+}
