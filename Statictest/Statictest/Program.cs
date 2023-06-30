5c97e390379174964876a7041535585f0789136b
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-30 09:05:59 +0530

    Add: Statictest

---
+++ Statictest/Statictest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Statictest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
