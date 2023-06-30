32149b5b302bfe7c3a6c13c61fa1b4e2cc1d627a
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-30 09:06:29 +0530

    Add: StringParse

---
+++ StringParse/StringParse/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StringParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "100";
+            string str2 = null;
+            Console.WriteLine(Convert.ToString(str));
+            Console.WriteLine(int.Parse(str));
+            Console.WriteLine(Convert.ToInt32(str2));
+            // It will give an error 
+            //Console.WriteLine(int.Parse(str2);
+        }
+    }
+}
