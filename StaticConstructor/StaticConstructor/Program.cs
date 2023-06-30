b62febe715c264314f4cdb73a6f7c859501f1db3
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-30 09:05:36 +0530

    Add: StaticConstructor

---
+++ StaticConstructor/StaticConstructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace StaticConstructor
+{
+    public class StaticConstructor
+    {
+        static StaticConstructor()
+        {
+            Console.WriteLine("Static Constructor Executed!");
+        }
+
+        StaticConstructor()
+        {
+            Console.WriteLine("This is Non Static Constructor");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Exceution Started...");
+            StaticConstructor obj = new();
+            Console.ReadKey();
+        }
+    }
+}
