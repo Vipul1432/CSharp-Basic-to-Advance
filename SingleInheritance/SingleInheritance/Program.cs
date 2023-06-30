a7b38b21f718cfba53ca43db636eab06241abb90
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-06-30 09:04:14 +0530

    Add: SingleInheritance

---
+++ SingleInheritance/SingleInheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace SingleInheritance
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Creating object of Child class and
+            // invoke the methods of Parent and Child classes
+            Cuboid obj = new Cuboid(2, 4, 6);
+            Console.WriteLine($"Volume is : {obj.Volume()}");
+            Console.WriteLine($"Area is : {obj.Area()}");
+            Console.WriteLine($"Perimeter is : {obj.Perimeter()}");
+            Console.ReadKey();
+        }
+    }
+    //Parent class
+    public class Rectangle
+    {
+        public int length;
+        public int breadth;
+        public int Area()
+        {
+            return length * breadth;
+        }
+        public int Perimeter()
+        {
+            return 2 * (length + breadth);
+        }
+    }
+
+    //Child Class
+    class Cuboid : Rectangle
+    {
+        public int height;
+        public Cuboid(int l, int b, int h)
+        {
+            length = l;
+            breadth = b;
+            height = h;
+        }
+        public int Volume()
+        {
+            return length * breadth * height;
+        }
+    }
+}
